8a7277eb564a3bb5e43f52465e65c4187b98f63c
Kadir Kadir <dragonrevoke@gmail.com>
2022-07-26 03:40:45 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Queue<int> input = new(Console.ReadLine().Split().Select(int.Parse));
+
+while (input.Any())
+{
+    Console.WriteLine(Out(input.Peek()));
+
+    input.Dequeue();
+}
+
+static string Out(int fb)
+{
+    if (fb % 15 == 0) {
+        return ("fizzbuzz");
+    }
+    else if (fb % 3 == 0) {
+        return "fizz";
+    }
+    else if (fb % 5 == 0) {
+        return "buzz";
+    }
+    
+    return fb.ToString();
+}
